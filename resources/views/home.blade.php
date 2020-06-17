c3d92aac936ad5a258aaa09312a938f637dde620
ayrton cravero <ayrtoncravero26@gmail.com>
2020-06-17 07:58:02 -0300

    add ProductoController, method home, view home, layouts -> app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <a href="#">Ver productos</a>
+        <a href="#">Emitir una factura</a>
+        <a href="#">Ver proveedores</a>
+        <a href="#">Ver categorías</a>
+        <a href="#">Dar de alta una compra</a>
+        <a href="#">Ver informes</a>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'ProductosController@home')->name('ProductosController@home');
+Route::post('/home', 'ProductosController@home')->name('ProductosController@home');
